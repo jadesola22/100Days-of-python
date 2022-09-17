ff1b072e5ea845c20dbb784b11ba3a8fa9a36ab6
jadesolar <>
2022-09-17 14:51:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Welcome to Ixora Analytics")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What is the name of the City you grew up in?\n")
+print(city)
+
+#3. Ask the user for the name of a pet.
+pet = input("The name of your Pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("You  brand  name  could  be " +  city  + " "  + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
